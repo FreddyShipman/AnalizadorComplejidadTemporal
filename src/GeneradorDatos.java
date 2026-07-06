3537121add531429a120abc5aba404ce6522a3fc
FreddyShipman <jose.guzman252524@potros.itson.edu.mx>
2026-07-06 14:45:39 -0700

    Actualizar GeneradorDatos y Main para generar y ordenar arreglos aleatorios

--- src/GeneradorDatos.java
+++ src/GeneradorDatos.java
@@ -1,6 +1,7 @@
 // Equipo 1:
 // José Alfredo Guzman Moreno - 252524
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class GeneradorDatos {
@@ -8,8 +9,14 @@ public class GeneradorDatos {
         int[] arreglo = new int[tamano];
         Random random = new Random();
         for (int i = 0; i < tamano; i++) {
-            arreglo[i] = random.nextInt(20) + 1;
+            arreglo[i] = random.nextInt(50) + 1;
         }
         return arreglo;
+    }
+
+    public static int[] ordenarArreglo(int[] arregloOriginal) {
+        int[] arregloOrdenado = Arrays.copyOf(arregloOriginal, arregloOriginal.length);
+        Arrays.sort(arregloOrdenado);
+        return arregloOrdenado;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Inicio de la prueba");
         int tamanoPrueba = 20;
-        int[] arregloDePrueba = GeneradorDatos.generarArregloAleatorio(tamanoPrueba);
-        System.out.println("Arreglo generado: ");        
-        System.out.println(Arrays.toString(arregloDePrueba));
+        int[] arregloDesordenado = GeneradorDatos.generarArregloAleatorio(tamanoPrueba);     
+        int[] arregloOrdenado = GeneradorDatos.ordenarArreglo(arregloDesordenado);
+        System.out.println("Arreglo desordenado:");
+        System.out.println(Arrays.toString(arregloDesordenado));
+        System.out.println("\nArreglo ordenado:");
+        System.out.println(Arrays.toString(arregloOrdenado));
     }
 }
