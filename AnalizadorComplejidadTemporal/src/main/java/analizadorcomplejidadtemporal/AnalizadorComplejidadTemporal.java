3aea000835f778d09810d5b1e0c681f5201c6182
FreddyShipman <jose.guzman252524@potros.itson.edu.mx>
2026-07-07 13:46:38 -0700

    Eliminacion de la prueba del arreglo
    Se comprobo que el metodo funciona, si se genera un arreglo aleatorio y ese mismo arreglo se ordena para el caso de busqueda binaria utilizando el algoritmo de ordenamiento quicksort

--- AnalizadorComplejidadTemporal/src/main/java/analizadorcomplejidadtemporal/AnalizadorComplejidadTemporal.java
+++ AnalizadorComplejidadTemporal/src/main/java/analizadorcomplejidadtemporal/AnalizadorComplejidadTemporal.java
@@ -18,15 +18,4 @@ public class AnalizadorComplejidadTemporal {
             ventana.setVisible(true);
         });
     }
-    
-//    public static void main(String[] args) {
-//        System.out.println("Inicio de la prueba");
-//        int tamanoPrueba = 20;
-//        int[] arregloDesordenado = GeneradorDatos.generarArregloAleatorio(tamanoPrueba);     
-//        int[] arregloOrdenado = GeneradorDatos.ordenarArreglo(arregloDesordenado);
-//        System.out.println("Arreglo desordenado:");
-//        System.out.println(Arrays.toString(arregloDesordenado));
-//        System.out.println("\nArreglo ordenado:");
-//        System.out.println(Arrays.toString(arregloOrdenado));
-//    }
 }
